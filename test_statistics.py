033a6bfb0f5cf82b400c0fe882185abe472025b3
Sittanat <apalakawongna@gmail.com>
2024-09-09 14:57:43 +0700

    unittest

--- test_statistics.py
+++ test_statistics.py
@@ -8,31 +8,23 @@ class StatisticsTest(TestCase):
     def test_variance_typical_values(self):
         """variance of typical values"""
         self.assertEqual(0.0, variance([10.0, 10.0, 10.0, 10.0, 10.0]))
-        self.assertEqual(2.0, variance([1, 2, 3, 4, 5]))
-        self.assertEqual(8.0, variance([10, 2, 8, 4, 6]))
+        self.assertAlmostEqual(2.0, variance([1, 2, 3, 4, 5]), places=7)
+        self.assertAlmostEqual(8.0, variance([10, 2, 8, 4, 6]), places=7)
 
     def test_variance_non_integers(self):
         """variance should work with decimal values"""
         # variance([x,y,z]) == variance([x+d,y+d,z+d]) for any d
-        self.assertAlmostEqual(
-            4.0, variance([0.1, 4.1]), places=7
-        )
+        self.assertAlmostEqual(4.0, variance([0.1, 4.1]), places=7)
         # variance([0,4,4,8]) == 8
-        self.assertAlmostEqual(
-            8.0, variance([0.1, 4.1, 4.1, 8.1]), places=7
-        )
+        self.assertAlmostEqual(8.0, variance([0.1, 4.1, 4.1, 8.1]), places=7)
 
     def test_stdev(self):
         # standard deviation of a single value should be zero
         self.assertEqual(0.0, stdev([10.0]))
         # simple test
-        self.assertAlmostEqual(
-            2.0, stdev([1, 5]), places=7
-        )
+        self.assertAlmostEqual(2.0, stdev([1, 5]), places=7)
         # variance([0, 0.5, 1, 1.5, 2.0]) is 0.5
-        self.assertAlmostEqual(
-            sqrt(0.5), stdev([0, 0.5, 1, 1.5, 2]), places=7
-        )
+        self.assertAlmostEqual(sqrt(0.5), stdev([0, 0.5, 1, 1.5, 2]), places=7)
 
 
 if __name__ == '__main__':
